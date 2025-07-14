38dcee89691624d3e0051d5b5d2c52f514f2c1ba
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-07-14 12:18:03 -0400

    feat: Add form toggle function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-const formBtn = document.querySelector('.display-form-btn');
-const dialog = document.querySelector('dialog');
-const closeBtn = document.querySelector('#close-dialog')
+function toggleFormModal() {
+    /** Attaches an event listener to the parent container of the modal and form.
+     * If the 'add book' button is clicked, the modal is revealed.
+     * If the 'cancel' button is clicked, the modal is closed.
+     */
+    
+    const formContainer = document.querySelector('.add-book-container');
+    const dialog = document.querySelector('dialog');
 
-formBtn.addEventListener('click', () => {
-    dialog.showModal();
-})
+    formContainer.addEventListener('click', (event) => {
+        if (event.target.classList.contains('display-form-btn')) {
+            dialog.showModal();
+        }
 
-closeBtn.addEventListener('click', () => {
-    dialog.close();
-})
+        else if (event.target.classList.contains('close-dialog')) {
+            dialog.close();
+        }
+    })
+}
+
+toggleFormModal();
