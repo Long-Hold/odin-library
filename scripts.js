5848ed457f18ea0c546538ee8dd934e4a72865da
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-07-14 13:33:30 -0400

    feat: Add Book object constructor

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
+function Book(title, author, numOfPages, readStatus) {
+    if (!new.target) {
+        throw Error('Must use New when creating Book Object');
+    }
+
+    // Generate a secure and random ID
+    this.bookID = self.crypto.randomUUID();
+
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.readStatus = readStatus;
+}
+
 function toggleFormModal() {
     /** Attaches an event listener to the parent container of the modal and form.
      * If the 'add book' button is clicked, the modal is revealed.
      * If the 'cancel' button is clicked, the modal is closed.
      */
-    
+
     const formContainer = document.querySelector('.add-book-container');
     const dialog = document.querySelector('dialog');
 
@@ -16,6 +30,10 @@ function toggleFormModal() {
             dialog.close();
         }
     })
+}
+
+function createNewBook() {
+
 }
 
 toggleFormModal();
