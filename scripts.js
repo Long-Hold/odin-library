5e19575c1895ddd819e53d480e4bc7f4851e7595
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-07-14 04:13:15 -0400

    feat: Add rudimentary dialog control

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const formBtn = document.querySelector('.display-form-btn');
+const dialog = document.querySelector('dialog');
+const closeBtn = document.querySelector('#close-dialog')
+formBtn.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+})
